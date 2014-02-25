b60687c279a14fde3f2f706bd68d4703ad685f8c
marwan-salem <ya dola ya 3rs>
2014-02-25 18:37:23 +0200

    merwesh el gamed fash5

--- queue1/Debug/subdir_rules.mk
+++ queue1/Debug/subdir_rules.mk
@@ -6,7 +6,7 @@
 main.obj: ../main.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: C6000 Compiler'
-	"C:/ti/C6000 Code Generation Tools 7.4.6/bin/cl6x" -mv6600 --abi=eabi -g --include_path="C:/ti/C6000 Code Generation Tools 7.4.6/include" --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv5/tools/compiler/c6000_7.4.4/bin/cl6x" -mv6600 --abi=eabi -g --include_path="C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include" --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
